a8e8c41e113dc2d1d5001cf03e55293fba782bf3
Vinh_DQ <53872492+Vinh217@users.noreply.github.com>
2024-08-01 17:20:44 +0700

    add library

--- src/layouts/header.jsx
+++ src/layouts/header.jsx
@@ -1,3 +1,15 @@
+import Marquee from "react-fast-marquee";
+
 export default function Header() {
-    return <div>Header</div>;
+    return (
+        <Marquee
+            speed={20}
+            autoFill={true}
+            className="h-[50px] bg-[#3498db] overflow-hidden"
+        >
+            <div className="flex items-center gap-[15px]">
+                <img src="/assets/favicon.ico" alt="hello" className="w-[23px] shrink-0"/>
+                <div className="text-[15px] font-yourFont text-text-white font-[700] mr-3">REHYDRATE</div>
+            </div>
+        </Marquee>);
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,21 +5,25 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    screens: {
-      'sm': '640px',
-      'md': '768px',
-      'lg': '1024px',
-      'xl': '1280px',
-      '2xl': '1536px'
+    extend: {
+      screens: {
+        'sm': '640px',
+        'md': '768px',
+        'lg': '1024px',
+        'xl': '1280px',
+        '2xl': '1536px'
+      },
+      colors: {
+        'bg-base': '#000000',
+        'text-white': '#ffffff'
+      },
+      boxShadow: {
+        'xs': '0px 4.282px 52.456px 0px rgba(0, 7, 72, 0.12)',
+      },
+      fontFamily: {
+        yourFont: ['font1', 'font2'],
+      }
     },
-    colors: {
-      'bg-base': '#000000',
-      'text-white': '#ffffff'
-    },
-    boxShadow: {
-      'xs': '0px 4.282px 52.456px 0px rgba(0, 7, 72, 0.12)',
-    },
-    extend: {},
   },
   plugins: [],
 }
